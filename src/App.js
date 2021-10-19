dd45f9aabe307447a4bfd5170fa7ad2c14e80f0c
Kumudperera <nethmalperera54@gmail.com>
2021-10-19 12:37:12 +0530

    First Component Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from './components/Todo';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Hello World!' />
+      <Todo text='Hello React!' />
+      <Todo text='Hello JavaScript!' />
+    </div>
+  );
 }
 
 export default App;
